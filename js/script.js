2752cf77903f19812dcd9fa0fe7de26419069569
haslozik <wojciechperlinskitpel@gmail.com>
2021-03-11 00:01:48 +0100

    ver 1.2 finished footer

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-/*var show = document.getElementById("mMenu");
-        function showMenu() {
-            show.style.right = '0';
-        }
-
-        function closeMenu() {
-            show.style.right = '-100%';
-        }
-*/
-
 var sliderConfig = {
     type: 'carousel',
     startAt: 0,
